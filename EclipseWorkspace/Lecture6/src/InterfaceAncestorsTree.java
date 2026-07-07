9180ea4d44ab28e49a70bf74e53fb1b75faa86f0
Psychodes254 <justusmasika80@gmail.com>
2026-07-07 16:19:45 +0300

    Lecture 6, reviewed the Ancestor Tree implementation with additon of female and year of birth check

---
+++ EclipseWorkspace/Lecture6/src/InterfaceAncestorsTree.java
@@ -0,0 +1,52 @@
+import tester.*;
+
+// to represent IAT
+interface IAT { }
+
+// to represent Unknown class
+class Unknown implements IAT {
+  Unknown() { }
+}
+
+// to represent Person class
+class Person implements IAT {
+  String name;
+  int yob;
+  IAT mom;
+  IAT dad;
+  boolean isMale;
+  
+  // the constructor
+  Person(String name, int yob, boolean isMale, IAT mom, IAT dad) {
+    this.name = name;
+    this.yob = yob;
+    this.isMale = isMale;
+    this.mom = mom;
+    this.dad = dad;
+  }
+}
+
+// examples and tests for the class hierarchy that represents IAT
+class ExamplesIAT {
+  IAT enid = new Person("Enid", 1904, false, new Unknown(), new Unknown());
+  IAT edward = new Person("Edward", 1902, true, new Unknown(), new Unknown());
+  IAT emma = new Person("Emma", 1906, false, new Unknown(), new Unknown());
+  IAT eustace = new Person("Eustace", 1907, true, new Unknown(), new Unknown());
+
+  IAT david = new Person("David", 1925, true, new Unknown(), this.edward);
+  IAT daisy = new Person("Daisy", 1927, false, new Unknown(), new Unknown());
+  IAT dana = new Person("Dana", 1933, false, new Unknown(), new Unknown());
+  IAT darcy = new Person("Darcy", 1930, false, this.emma, this.eustace);
+  IAT darren = new Person("Darren", 1935, true, this.enid, new Unknown());
+  IAT dixon = new Person("Dixon", 1936, true, new Unknown(), new Unknown());
+
+  IAT clyde = new Person("Clyde", 1955, true, this.daisy, this.david);
+  IAT candace = new Person("Candace", 1960, false, this.dana, this.darren);
+  IAT cameron = new Person("Cameron", 1959, true, new Unknown(), this.dixon);
+  IAT claire = new Person("Claire", 1956, false, this.darcy, new Unknown());
+
+  IAT bill = new Person("Bill", 1980, true, this.candace, this.clyde);
+  IAT bree = new Person("Bree", 1981, false, this.claire, this.cameron);
+
+  IAT andrew = new Person("Andrew", 2001, true, this.bree, this.bill);
+}
