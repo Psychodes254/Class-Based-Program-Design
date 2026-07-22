7c34ee46a460d99da57af23062b9f6f2ef428b04
Psychodes254 <justusmasika80@gmail.com>
2026-07-22 19:44:09 +0300

    A River System with mouth, sources, and Confluences for two rivers joining from left or right

---
+++ EclipseWorkspace/Assignment2/src/RiverSystem.java
@@ -0,0 +1,232 @@
+import tester.*;
+
+// the end of a river
+class Mouth{
+  Location loc;
+  IRiver river;
+  
+  Mouth(Location loc, IRiver river){
+    this.loc = loc;
+    this.river = river;
+  }
+  
+  int sources() {
+    return this.river.sources();
+  }
+  
+  public boolean onRiver(Location aloc) {
+    return this.loc.sameLoc(aloc) ||
+           this.river.onRiver(aloc);
+  }
+  
+  //the total length of the river system
+  int length(){
+  return this.river.length();
+  }
+}
+
+// a location on a river
+class Location{
+  int x;
+  int y;
+  String name;
+  
+  Location(int x, int y, String name){
+    this.x = x;
+    this.y = y;
+    this.name = name;
+  }
+  
+  boolean sameLoc(Location aloc) {
+    return (this.x == aloc.x && 
+           this.y == aloc.y);
+  }
+}
+
+// a river system
+interface IRiver{ 
+  //count the number of sources
+  //for this river system
+  int sources();
+  
+  boolean onRiver(Location aloc);
+  
+  //compute the total length of the
+  //waterways that flow into this point
+  int length();
+}
+
+// a source a river
+class Sources implements IRiver{
+  Location loc;
+  int miles;
+  
+  Sources(Location loc, int miles){
+    this.loc = loc;
+    this.miles = miles;
+  }
+  
+  public int sources() {
+    return 1;
+  }
+  
+  public boolean onRiver(Location aloc) {
+    return this.loc.sameLoc(aloc);
+  }
+  
+  public int length(){
+    return this.miles;
+    }
+}
+
+// a confluence of two rivers
+class Confluence implements IRiver{
+  Location loc;
+  IRiver left;
+  IRiver right;
+  int miles;
+  
+  Confluence(Location loc, IRiver left, IRiver right, int miles){
+    this.loc = loc;
+    this.left = left;
+    this.right = right;
+    this.miles = miles;
+  }
+  
+  public int sources() {
+    return this.left.sources() + this.right.sources();
+  }
+  
+  public boolean onRiver(Location aloc) {
+    return this.loc.sameLoc(aloc) ||
+           this.left.onRiver(aloc) ||
+           this.right.onRiver(aloc);
+  }
+  
+  public int length(){
+    return this.miles + 
+           this.left.length() + 
+           this.right.length();
+    }
+}
+
+class ExamplesRiver {
+
+  // Locations
+  Location sourceA = new Location(0, 10, "Source A");
+  Location sourceB = new Location(10, 10, "Source B");
+  Location sourceC = new Location(20, 10, "Source C");
+
+  Location fork1 = new Location(5, 5, "Fork 1");
+  Location fork2 = new Location(15, 3, "Fork 2");
+
+  Location mouthLoc = new Location(10, 0, "Mouth");
+
+  Location outside = new Location(50, 50, "Outside");
+
+  // Sources
+  IRiver riverA = new Sources(this.sourceA, 8);
+  IRiver riverB = new Sources(this.sourceB, 6);
+  IRiver riverC = new Sources(this.sourceC, 10);
+
+  // Confluences
+  IRiver upperRiver =
+      new Confluence(this.fork1,
+          this.riverA,
+          this.riverB,
+          4);
+
+  IRiver wholeRiver =
+      new Confluence(this.fork2,
+          this.upperRiver,
+          this.riverC,
+          5);
+
+  // Mouth
+  Mouth mouth =
+      new Mouth(this.mouthLoc, this.wholeRiver);
+  
+  boolean testSameLoc(Tester t) {
+    return t.checkExpect(
+        this.sourceA.sameLoc(new Location(0, 10, "Another")),
+        true)
+        &&
+
+        t.checkExpect(
+            this.sourceA.sameLoc(this.sourceB),
+            false)
+        &&
+
+        t.checkExpect(
+            this.mouthLoc.sameLoc(this.mouthLoc),
+            true);
+  }
+  
+  boolean testSources(Tester t) {
+    return t.checkExpect(this.riverA.sources(), 1)
+        &&
+
+        t.checkExpect(this.upperRiver.sources(), 2)
+        &&
+
+        t.checkExpect(this.wholeRiver.sources(), 3)
+        &&
+
+        t.checkExpect(this.mouth.sources(), 3);
+  }
+  
+  boolean testLength(Tester t) {
+    return t.checkExpect(this.riverA.length(), 8)
+        &&
+
+        t.checkExpect(this.upperRiver.length(), 18)
+        &&
+
+        t.checkExpect(this.wholeRiver.length(), 33)
+        &&
+
+        t.checkExpect(this.mouth.length(), 33);
+  }
+  
+  boolean testOnRiver(Tester t) {
+    return t.checkExpect(
+        this.riverA.onRiver(this.sourceA),
+        true) &&
+        t.checkExpect(
+            this.upperRiver.onRiver(this.sourceA),
+            true) &&
+        t.checkExpect(
+            this.upperRiver.onRiver(this.sourceB),
+            true) &&
+        t.checkExpect(
+            this.upperRiver.onRiver(this.fork1),
+            true) &&
+        t.checkExpect(
+            this.upperRiver.onRiver(this.sourceC),
+            false) &&
+        t.checkExpect(
+            this.wholeRiver.onRiver(this.sourceC),
+            true) &&
+        t.checkExpect(
+            this.wholeRiver.onRiver(this.fork2),
+            true) &&
+        t.checkExpect(
+            this.wholeRiver.onRiver(this.outside),
+            false) &&
+        t.checkExpect(
+            this.mouth.onRiver(this.mouthLoc),
+            true) &&
+        t.checkExpect(
+            this.mouth.onRiver(this.sourceA),
+            true) &&
+        t.checkExpect(
+            this.mouth.onRiver(this.sourceB),
+            true)  &&
+        t.checkExpect(
+            this.mouth.onRiver(this.sourceC),
+            true) &&
+        t.checkExpect(
+            this.mouth.onRiver(this.outside),
+            false);
+  }
+}
