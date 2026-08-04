87d678484f8c78f298f7b50b1ba23a39af74d0fe
Psychodes254 <justusmasika80@gmail.com>
2026-08-04 16:03:00 +0300

    Vehicle management data definition with Vehicle as Super-class, and Sedan as Sub-class

--- EclipseWorkspace/Assignment3/src/VehicleManagement.java
+++ EclipseWorkspace/Assignment3/src/VehicleManagement.java
@@ -1,4 +1,17 @@
+// to represent class class Vehicle
+class Vehicle{
+  int mileage;
+  int price;
+  
+  Vehicle(int mileage, int price){
+    this.mileage = mileage;
+    this.price = price;
+  }
+}
 
-public class VehicleManagement {
-
+// to represent class Sedan
+class Sedan extends Vehicle{
+  Sedan(int mileage, int price){
+    super(mileage, price);
+  }
 }
