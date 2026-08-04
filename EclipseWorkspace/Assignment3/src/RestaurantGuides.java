7e6ac01aa0e51d24dbba05b279d976587c2f11b6
Psychodes254 <justusmasika80@gmail.com>
2026-08-04 16:01:05 +0300

    A Restaurant Guide implemented using the commonality rule between Super-class and Sub-class

--- EclipseWorkspace/Assignment3/src/RestaurantGuides.java
+++ EclipseWorkspace/Assignment3/src/RestaurantGuides.java
@@ -1,4 +1,27 @@
+// to represent class Place
+class Place{
+  
+}
 
-public class RestaurantGuides {
+// to represent class Restaurant
+class Restaurant{
+  String name;
+  String price;
+  Place place;
+  
+  Restaurant(String name, String price, Place place){
+    this.name = name;
+    this.price = price;
+    this.place = place;
+  }
+}
 
+// to represent class ChineseRestaurant
+class ChineseRestaurant extends Restaurant{
+  boolean usesMSG;
+  
+  ChineseRestaurant(String name, String price, Place place, boolean usesMSG){
+    super(name, price, place);
+    this.usesMSG = usesMSG;
+  }
 }
