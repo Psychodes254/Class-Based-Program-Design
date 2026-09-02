f37726e9dede940448761160c23ecefd11d198a4
Psychodes254 <justusmasika80@gmail.com>
2026-09-02 22:08:17 +0300

    feat: implement Counter class with local and global variables to perform mutation

---
+++ EclipseWorkspace/Lecture17/src/Counter.java
@@ -0,0 +1,37 @@
+import tester.*;
+
+class Counter{
+  int val;
+  
+  Counter(){
+    this(0);
+  }
+  
+  Counter(int initialVal){
+    this.val = initialVal;
+  }
+  
+  int get() {
+    int ans = val;
+    this.val = val + 1;
+    return ans;
+  }
+}
+
+class ExamplesCounter{
+  ExamplesCounter(){}
+  
+  Counter counter1 = new Counter();
+  Counter counter2 = new Counter(2);
+  
+  boolean testCounter(Tester t) {
+    return
+        t.checkExpect(this.counter1.get(), 0) &&
+        t.checkExpect(this.counter2.get(), 2) &&
+        t.checkExpect(this.counter1.get() == counter1.get(), false) &&
+        t.checkExpect(this.counter1.get() == counter2.get(), true) &&
+        t.checkExpect(this.counter2.get() == counter1.get(), true) &&
+        t.checkExpect(this.counter2.get() == counter2.get(), false) &&
+        t.checkExpect(this.counter2.get() == counter1.get(), false);
+  }
+}
